8d655807c0dda2d59671207fddebbb93132e622c
Anvar <www.latvienhoasadrizeges@gmail.com>
2020-09-28 16:06:48 +0500

    fine

--- js/script.js
+++ js/script.js
@@ -7,13 +7,46 @@ $(document).ready(function () {
             $('.answer').removeClass('open');
             $(this).next().addClass('open');
             $(this).find('.fa-plus').css("display", "none");
-            
+
         } else {
             $('.answer').slideUp();
             $('.answer').removeClass('open');
             $(this).find('.fa-plus').css("display", "inline-block");
         }
     })
+
+    document.querySelector('.left').onclick = left;
+    document.querySelector('.right').onclick = right;
+    var inner = document.querySelector('.carousel-inner');
+    var move = 0;
+    var timer;
+    autoplay();
+
+    function autoplay() {
+        timer = setTimeout(left, 3000);
+    }
+
+    function left() {
+        move -= 300;
+        if (move < -1200) {
+            move = 0;
+        }
+        inner.style.left = move + 'px';
+        clearTimeout(timer);
+        autoplay();
+    }
+    function right() {
+        if (move == 0) {
+            move = -1200;
+        }
+        else {
+            move += 300;
+        }
+        inner.style.left = move + 'px';
+        clearTimeout(timer);
+        autoplay();
+    }
+
 })
 
 
